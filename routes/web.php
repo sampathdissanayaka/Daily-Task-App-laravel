dd44534e97bd39ca5aaa8bec6758dcac2c4445d5
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-17 15:01:32 +0700

    create a controller

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/aboutus',function() {
+Route::get('/about',function() {
     return view('aboutus');
+});
+
+Route ::get('/login',function(){
+    echo "welcome to laravel!";
+});
+
+Route::get('/contact',function(){
+     return view('contactus');
 });
