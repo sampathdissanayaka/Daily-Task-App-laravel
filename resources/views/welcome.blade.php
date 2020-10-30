e247076bbabb2be89420a728a1939419e90e5685
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-30 19:48:28 +0700

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <title>Daliy Task</title>
+    <style>
+       h1{
+           margin-top:150px;
+       }
+    
+    </style>
+    <title>Daily Task</title>
 </head>
 <body>
- <div class="container">
+ <div class="container" >
    <div class="text-center">
-    <h1>Daliy Task App</h>
-   </div>
+    <h1>DAILY TASK APP</h>
+  
+   <br><br>
+   <a href="/task" class="btn btn-primary">View Tasks</a>
+ </div>
  </div>   
 </body>
 </html>
